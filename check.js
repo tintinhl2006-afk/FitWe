089e1bdd22508efd478d83d46ce8da503827054a
martin <martinherrerolitarte@gmail.com>
2026-05-21 14:43:29 +0200

    puesta a punto

--- check.js
+++
@@ -1,3 +0,0 @@
-const { PrismaClient } = require('@prisma/client'); 
-const prisma = new PrismaClient(); 
-prisma.foodItem.findMany().then(f => console.log(f.length)).finally(() => prisma.$disconnect());
